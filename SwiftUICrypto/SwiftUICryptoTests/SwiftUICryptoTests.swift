5c25fa77c8559f5ce0eecbc07f138c8b7bf22037
Tung Vu <tungvt.it.92@gmail.com>
2025-04-11 15:43:17 +0900

    Initialize project

---
+++ SwiftUICrypto/SwiftUICryptoTests/SwiftUICryptoTests.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftUICryptoTests.swift
+//  SwiftUICryptoTests
+//
+
+import Testing
+@testable import SwiftUICrypto
+
+struct SwiftUICryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
