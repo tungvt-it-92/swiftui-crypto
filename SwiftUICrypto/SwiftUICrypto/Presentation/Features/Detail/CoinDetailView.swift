39efa925c1de9fe83cbd2e6c25d5e54f429774c1
Tung Vu <tungvt.it.92@gmail.com>
2025-04-26 16:31:56 +0900

    Show coin description at coin detail screen

--- SwiftUICrypto/SwiftUICrypto/Presentation/Features/Detail/CoinDetailView.swift
+++ SwiftUICrypto/SwiftUICrypto/Presentation/Features/Detail/CoinDetailView.swift
@@ -8,6 +8,7 @@ import SwiftUI
 struct CoinDetailView: View {
     var coin: CoinModel
     @StateObject private var coinDetailViewModel: CoinDetailViewModel
+    @State private var readMoreDescription: Bool = true
     
     private let columns: [GridItem] = [
         GridItem(.flexible()),
@@ -24,20 +25,22 @@ struct CoinDetailView: View {
             VStack {
                 ChartView(coin: coinDetailViewModel.coin)
                     .frame(height: 250)
-                    
+                
                 Group {
                     overviewViews
                     additionalViews
+                    linksView
                 }
                 .padding(.horizontal, 10)
             }
             .padding(.top, 50)
         }
-        .navigationTitle(coinDetailViewModel.coin.name)
-        .navigationBarTitleDisplayMode(.large)
         .toolbar {
             navigationTrailingItem
         }
+        .navigationTitle(coinDetailViewModel.coin.name)
+        .navigationBarTitleDisplayMode(.large)
+        .animation(.easeInOut(duration: 0.3), value: readMoreDescription)
         .onAppear {
             coinDetailViewModel.fetchCoinDetail()
         }
@@ -57,6 +60,8 @@ extension CoinDetailView {
                     .foregroundStyle(Color.theme.accentColor)
                     .frame(maxWidth: .infinity, alignment: .leading)
                 Divider()
+                
+                descriptionView
                 
                 LazyVGrid(
                     columns: columns,
@@ -109,6 +114,46 @@ extension CoinDetailView {
                     .frame(width: 25, height: 25)
             }
         }
+    }
+    
+    @ViewBuilder private var descriptionView: some View {
+        if let coinDescription = coinDetailViewModel.coinDescription,
+           !coinDescription.isEmpty {
+            VStack(alignment: .leading) {
+                Text(coinDescription)
+                    .font(.caption)
+                    .foregroundColor(Color.theme.secondaryTextColor)
+                    .lineLimit(readMoreDescription ? 3 : nil)
+                Button {
+                    readMoreDescription.toggle()
+                } label: {
+                    Text(readMoreDescription ? "Read more..." : "Read less")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.blue)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        } else {
+            EmptyView()
+        }
+    }
+    
+    private var linksView: some View {
+        VStack(alignment: .leading) {
+            if let homepageUrlString = coinDetailViewModel.homepageUrl,
+               let homepageUrl = URL(string: homepageUrlString) {
+                SwiftUI.Link("Webstite", destination: homepageUrl)
+            }
+            
+            if let redditUrlString = coinDetailViewModel.redditUrl,
+               let redditUrl = URL(string: redditUrlString) {
+                SwiftUI.Link("Reddit", destination: redditUrl)
+            }
+        }
+        .tint(.blue)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.subheadline)
     }
 }
 
